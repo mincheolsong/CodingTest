1425dffec77ee8afc48bde4a10065d20d73394f2
mincheolsong <80660585+mincheolsong@users.noreply.github.com>
2023-11-22 22:53:44 +0900

    [Platinum V] Title: 백조의 호수, Time: 968 ms, Memory: 206924 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.java
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.java
@@ -1,103 +1,116 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 
 public class Main {
 
-	static int r,c,ex,ey;
-	static char[][] map;
-	static boolean[][] check;
-	static Queue<int[]> wq,sq;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		map = new char[r][c];
-		wq = new LinkedList<>();
-		sq = new LinkedList<>();
-		int sx = -1, sy = -1;
-		int idx=0;
-		for(int i=0; i<r; i++) {
-			String line = br.readLine();
-			for(int j=0; j<c; j++) {
-				map[i][j] = line.charAt(j);
-				if(map[i][j] == 'L') {
-					if(sx==-1 && sy==-1) {
-						sx = j;
-						sy = i;
-					}else {
-						ex = j;
-						ey = i;
-					}
-					map[i][j] ='.';
-				}
-				
-				if(map[i][j] == '.') {
-					wq.add(new int[] {j,i});
-				}
-			}
-		}
-		
-		check = new boolean[r][c];
-		sq.add(new int[] {sx,sy});
-		check[sy][sx] = true;
-		
-		int time=0;
-		while(true) {
-			if(move()) break;
-			melting();
-			time++;
-			
-		}
-		System.out.println(time);
-	}
-	
-	static boolean move() {
-		Queue<int[]> q = new LinkedList<>();
-		
-		while(!sq.isEmpty()) {
-			int[] p = sq.poll();
-			int px = p[0], py = p[1];
-			if(px == ex && py == ey) {
-				return true;
-			}
-			
-			for(int i=0; i<4; i++) {
-				int nx = px + dx[i];
-				int ny = py + dy[i];
-				
-				if(nx <0 || ny<0 || nx>c-1 || ny>r-1 || check[ny][nx]) continue;
-				
-				check[ny][nx] = true;
-				if(map[ny][nx] == '.') {
-					sq.add(new int[] {nx,ny}); 
-				}else if(map[ny][nx] == 'X') { // 다음 탐색지역 
-					q.add(new int[] {nx,ny});
-				}
-			}
-		}
-		
-		sq = q;
-		return false;
-	}
-	
-	static void melting() {
-		int size = wq.size();
-		for(int i=0; i<size; i++) {
-			int[] p = wq.poll();
-			
-			for(int d=0; d<4; d++) {
-				int nx = p[0] + dx[d];
-				int ny = p[1] + dy[d];
-				
-				if(nx <0 || ny<0 || nx>c-1 || ny>r-1) continue;
-				if(map[ny][nx] == 'X') {
-					map[ny][nx] = '.';
-					wq.add(new int[] {nx,ny});
-				}
-			}
-		}
-	}
+    static int R,C;
+    static int er,ec; // 백조 위치
+    static boolean[][] chk; // 백조 bfs에 사용
+    static char[][] map;
+    static Queue<int[]> bq; // 백조 bfs에 사용하는 큐
+    static Queue<int[]> wq; // 물의 위치를 저장하는 큐
+    static int[] dr = {-1,0,1,0};
+    static int[] dc = {0,1,0,-1};
+
+
+    static boolean move(){
+        Queue<int[]> tmp = new LinkedList<>();
+
+        while(!bq.isEmpty()){
+
+            int[] c = bq.poll();
+            int cr = c[0], cc=c[1];
+
+            if(cr==er && cc == ec){ // 상대 백조의 위치에 닿으면 return true
+                return true;
+            }
+
+            for(int d=0;d<4;d++){
+                int nr = c[0] + dr[d];
+                int nc = c[1] + dc[d];
+
+                if(nr<0 || nr>=R) continue;
+                if(nc<0 || nc>=C) continue;
+                if(chk[nr][nc]) continue;
+
+                chk[nr][nc]=true;
+                if(map[nr][nc]=='.'){
+                    bq.add(new int[]{nr,nc});
+                }else if (map[nr][nc]=='X'){ // 막혔으면 다음이동 시작위치 큐(tmp)에 저장
+                    tmp.add(new int[]{nr,nc});
+                }
+            }
+        }
+
+        bq = tmp; // 큐를 다음위치 큐로 갱신
+        return false;
+    }
+
+    static void melting(){
+        int size = wq.size();
+
+        for(int i=0;i<size;i++){ // 한 턴씩 bfs를 수행하기 위함 (물은 time 당 한번씩 번져야 하니까)
+            int[] c = wq.poll();
+            for(int d=0;d<4;d++){
+                int nr = c[0] + dr[d];
+                int nc = c[1] + dc[d];
+                if(nr<0 || nr>=R) continue;
+                if(nc<0 || nc>=C) continue;
+                if(map[nr][nc]=='X'){ // 얼음과 닿아있는 물의 좌표를 큐에 저장
+                    map[nr][nc]='.';
+                    wq.add(new int[]{nr,nc});
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new char[R][C];
+        bq = new LinkedList<>();
+        wq = new LinkedList<>();
+        int sr=-1, sc = -1;
+
+
+        for(int i=0;i<R;i++){
+            String s = br.readLine();
+            for(int j=0;j<C;j++){
+                map[i][j] = s.charAt(j);
+                if(map[i][j]=='L'){
+                    if(sr==-1 && sc==-1){
+                        sr = i;
+                        sc = j;
+                    }else{
+                        er = i;
+                        ec = j;
+                    }
+                    map[i][j]='.'; // 백조의 좌표도 물 (백조도 물 위에 있다)
+                }
+                if(map[i][j]=='.'){ // 물의 위치 큐 초기화 (처음에는 모든 물의 좌표를 넣음, 나중엔 얼음과 닿아있는 좌표만 저장될 예정)
+                    wq.add(new int[]{i,j});
+                }
+            }
+        }
+        chk = new boolean[R][C];
+        bq.add(new int[]{sr,sc});
+        chk[sr][sc]=true;
+
+        int time=0;
+
+        while(true){
+            melting();
+            if(move()) break;
+            time++;
+        }
+
+        System.out.println(time+1); // time을 +1 해줘야 함
+
+    }
+
 }
