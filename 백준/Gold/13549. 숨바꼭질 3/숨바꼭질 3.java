09578e0a798526b534493a1c2ea71a708a56faf4
mincheolsong <80660585+mincheolsong@users.noreply.github.com>
2023-12-15 23:48:34 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 192 ms, Memory: 21676 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    static int N,K;
+    static int[] memo;
+    static Deque<int[]> q = new ArrayDeque<>();
+
+    static void bfs(){
+        while(!q.isEmpty()){
+            int[] cur = q.pollFirst();
+            int idx = cur[0];
+            int w = cur[1];
+
+            if(idx-1>=0 && (memo[idx-1]==-1 || w+1 < memo[idx-1])){
+                q.offer(new int[]{idx-1,w+1});
+                memo[idx-1]=w+1;
+            }
+            if(idx+1<=100001 && (memo[idx+1]==-1 || w+1 < memo[idx+1])){
+                q.offer(new int[]{idx+1,w+1});
+                memo[idx+1]=w+1;
+            }
+
+            if(idx*2 <= 100001 &&  (memo[idx*2]==-1 || w < memo[idx*2])){
+                q.offer(new int[]{idx*2,w});
+                memo[idx*2] = w;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        if (N >= K) {
+            System.out.println(N - K);
+        } else {
+            memo = new int[100002];
+            Arrays.fill(memo, -1);
+            memo[N] = 0;
+            q.offer(new int[]{N, 0});
+            bfs();
+            System.out.println(memo[K]);
+        }
+
+    }
+
+}
