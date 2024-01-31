3053559b457094ed89c56c877f76052730e875f1
mincheolsong <80660585+mincheolsong@users.noreply.github.com>
2024-01-31 15:19:26 +0900

    [Gold II] Title: 보석 도둑, Time: 1716 ms, Memory: 118504 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -2,8 +2,7 @@ import java.io.*;
 import java.util.*;
 
 
-class Jewel implements Comparable<Jewel>{
-
+class Jewel{
     int m,v;
 
     public Jewel(int m,int v){
@@ -11,46 +10,30 @@ class Jewel implements Comparable<Jewel>{
         this.v = v;
     }
 
-    @Override
-    public String toString(){
-        return "[ m : " + m + ", v : " + v + " ]";
-    }
-
-    @Override
-    public int compareTo(Jewel o){
-        if(this.m==o.m){
-            return -(this.v-o.v);
-        }
-        return this.m-o.m;
-    }
 
 }
-
 
 public class Main {
 
 
-
     static int N,K;
-    static long ans;
-    static Integer[] backpack;
-    static Jewel[] jewels;
-
+    static Jewel[] jewel;
+    static int[] bag;
+    static long answer;
 
     static void solve(){
 
-        int j = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2)->-(o1-o2));
 
+        int j = 0;
         for(int i=0;i<K;i++){
-
-            while(j<N && jewels[j].m <= backpack[i]){
-                pq.offer(jewels[j].v);
+            while(j<N && bag[i] >= jewel[j].m){
+                pq.offer(jewel[j].v);
                 j+=1;
             }
 
             if(!pq.isEmpty()){
-                ans += pq.poll();
+                answer += pq.poll();
             }
         }
 
@@ -62,29 +45,29 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        jewels = new Jewel[N];
-        backpack = new Integer[K];
-
-        ans = 0;
+        jewel = new Jewel[N];
+        bag = new int[K];
+        answer = 0;
 
         for(int i=0;i<N;i++){
-            int m,v;
             st = new StringTokenizer(br.readLine());
+            int m,v;
             m = Integer.parseInt(st.nextToken());
             v = Integer.parseInt(st.nextToken());
-            jewels[i] = new Jewel(m,v);
+            jewel[i] = new Jewel(m,v);
         }
+
+        Arrays.sort(jewel,(o1,o2)->(o1.m-o2.m));
 
         for(int i=0;i<K;i++){
-            backpack[i] = Integer.parseInt(br.readLine());
+            bag[i]=Integer.parseInt(br.readLine());
         }
-
-        Arrays.sort(jewels);
-        Arrays.sort(backpack);
+        Arrays.sort(bag);
 
         solve();
 
-        System.out.println(ans);
+        System.out.println(answer);
+
 
 
     }
