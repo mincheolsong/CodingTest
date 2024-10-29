db2b4c6ad2a33524f7dd9be62c786c1bd40bb53e
mincheolsong <80660585+mincheolsong@users.noreply.github.com>
2024-10-29 21:08:14 +0900

    [Silver III] Title: N과 M (1), Time: 1696 ms, Memory: 69004 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,18 +1,19 @@
 
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
     static int N,M;
-    static List<Integer> ans;
+    static int[] result;
     static boolean[] visited;
 
     static void solve(int start, int cnt, int target){
 
         if(cnt==target){
-            for(int i=0;i<ans.size();i++){
-                System.out.print(ans.get(i) + " ");
+            for(int i=0;i<M;i++){
+                System.out.print(result[i]+ " ");
             }
             System.out.println();
             return;
@@ -21,10 +22,9 @@ public class Main {
         for(int i=1;i<=N;i++){
             if(visited[i]) continue;
 
-            ans.add(i);
+            result[cnt] = i;
             visited[i] = true;
             solve(i+1,cnt+1,target);
-            ans.remove(ans.size()-1);
             visited[i] = false;
         }
 
@@ -36,7 +36,7 @@ public class Main {
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        ans = new ArrayList<>();
+        result = new int[M];
         visited = new boolean[N+1];
 
         solve(1,0,M);
