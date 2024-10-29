d6bce7fc013e2ad7f200804452ea3d426c95fdf4
mincheolsong <80660585+mincheolsong@users.noreply.github.com>
2024-10-29 21:26:15 +0900

    [Silver III] Title: N과 M (3), Time: 372 ms, Memory: 111804 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,44 @@
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N,M;
+    static int[] result;
+    static StringBuilder sb;
+    static void solve(int cnt){
+
+        if(cnt==M){
+            for(int i=0;i<M;i++){
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1;i<=N;i++){
+            result[cnt] = i;
+            solve(cnt+1);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        result = new int[M];
+        sb = new StringBuilder();
+
+        solve(0);
+
+        System.out.println(sb.toString());
+
+
+    }
+}
+
